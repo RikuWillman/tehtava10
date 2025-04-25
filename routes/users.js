7e6e85dd6455dc4acd8b0db24d60896359bbf3dc
riku, willman <AB6906@student.jamk.fi>
2025-04-25 20:33:03 +0300

    Replace bcrypt with bcryptjs for compatibility

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/usercontroller');
 const createToken = require('../createtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req, res) => {
   try {
